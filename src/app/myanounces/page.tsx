c361b25b158f5f9c932c206c0ff846ba4237b947
MathLevi20 <matheuslevi545@gmail.com>
2023-10-01 02:53:14 -0300

    feat: add card on myanounces

--- src/app/myanounces/page.tsx
+++ src/app/myanounces/page.tsx
@@ -32,79 +32,85 @@ const FilteredItems = () => {
   });
 
   return (
-    <div>
+    <div className=" ">
       <WithSubnavigation />
-      <div className="grid grid-cols-2 ">
-        <div className=" bg-white h-fit w-fit shadow-lg p-10 m-10 mx-20 rounded-md">
-          <div className="mb-4 ">
-            <label htmlFor="typeSelect" className="mr-2">
-              Filtrar por Tipo:
-            </label>
-            <select
-              id="typeSelect"
-              onChange={(e) => setSelectedType(e.target.value)}
-            >
-              <option value="all">Todos</option>
-              <option value="Papel">Papel</option>
-              <option value="Plástico">Plástico</option>
-              <option value="Vidro">Vidro</option>
-              {/* Adicione outras opções de tipo aqui */}
-            </select>
-          </div>
-
-          <div className="mb-4 grid grid-rows-2">
-            <label className="mr-2">Filtrar por Status:</label>
-            <div>
-              <button
-                className={`mr-2 px-4 py-2 border ${
-                  selectedStatus === "all" ? "bg-blue-500 text-white" : ""
-                }`}
-                onClick={() => setSelectedStatus("all")}
+      <div className=" ">
+        <div className="grid grid-cols-2 h-full ">
+          <div className=" bg-white h-fit w-fit shadow-lg p-10 m-10 mx-20 rounded-md">
+            <div className="mb-4 ">
+              <label htmlFor="typeSelect" className="mr-2">
+                Filtrar por Tipo:
+              </label>
+              <select
+                id="typeSelect"
+                onChange={(e) => setSelectedType(e.target.value)}
               >
-                Todos
-              </button>
-              <button
-                className={`mr-2 px-4 py-2 border ${
-                  selectedStatus === "completo" ? "bg-blue-500 text-white" : ""
-                }`}
-                onClick={() => setSelectedStatus("completo")}
-              >
-                Completo
-              </button>
-              <button
-                className={`mr-2 px-4 py-2 border ${
-                  selectedStatus === "anunciado" ? "bg-blue-500 text-white" : ""
-                }`}
-                onClick={() => setSelectedStatus("anunciado")}
-              >
-                Anunciado
-              </button>
-              <button
-                className={`mr-2 px-4 py-2 border ${
-                  selectedStatus === "negociando"
-                    ? "bg-blue-500 text-white"
-                    : ""
-                }`}
-                onClick={() => setSelectedStatus("negociando")}
-              >
-                Negociando
-              </button>
-              {/* Adicione outros botões de status conforme necessário */}
+                <option value="all">Todos</option>
+                <option value="Papel">Papel</option>
+                <option value="Plástico">Plástico</option>
+                <option value="Vidro">Vidro</option>
+                {/* Adicione outras opções de tipo aqui */}
+              </select>
             </div>
-          </div>
-        </div>
-        <div className="my-auto h-full">
-          {filteredItems.map((item: any) => (
-            <div
-              className="max-w-sm rounded overflow-hidden p-10 shadow-md"
-              key={item.id}
-            >
+
+            <div className="mb-4 grid grid-rows-2">
+              <label className="mr-2">Filtrar por Status:</label>
               <div>
-                <p>Tipo: {item.type} </p>
-                <p>Status:{item.status} </p>
+                <button
+                  className={`mr-2 px-4 py-2 border ${
+                    selectedStatus === "all" ? "bg-blue-500 text-white" : ""
+                  }`}
+                  onClick={() => setSelectedStatus("all")}
+                >
+                  Todos
+                </button>
+                <button
+                  className={`mr-2 px-4 py-2 border ${
+                    selectedStatus === "completo"
+                      ? "bg-blue-500 text-white"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedStatus("completo")}
+                >
+                  Completo
+                </button>
+                <button
+                  className={`mr-2 px-4 py-2 border ${
+                    selectedStatus === "anunciado"
+                      ? "bg-blue-500 text-white"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedStatus("anunciado")}
+                >
+                  Anunciado
+                </button>
+                <button
+                  className={`mr-2 px-4 py-2 border ${
+                    selectedStatus === "negociando"
+                      ? "bg-blue-500 text-white"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedStatus("negociando")}
+                >
+                  Negociando
+                </button>
+                {/* Adicione outros botões de status conforme necessário */}
               </div>
             </div>
-          ))}
+          </div>
+          <div className="my-auto h-full mt-10">
+            {filteredItems.map((item: any) => (
+              <div
+                className=" rounded bg-slate-50 m-4 overflow-hidden p-10 shadow-md"
+                key={item.id}
+              >
+                <div>
+                  <p>Tipo: {item.type} </p>
+                  <p>Status:{item.status} </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
