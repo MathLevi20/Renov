80f19f839f02d6076f5ead403ead1d298818274c
Weslley-David <weslleyvwp@hotmail.com>
2023-11-01 15:15:17 -0300

    feat: DetailAnounce

--- src/app/detailAnounce/page.tsx
+++ src/app/detailAnounce/page.tsx
@@ -1,7 +1,10 @@
-// pages/post.tsx
+"use client";
+
+import { BASE_URL } from "@/utils/API";
 import { useEffect, useState } from 'react';
-// interfaces/PostData.ts
-interface PostData {
+import axios from 'axios';
+
+interface Anounce {
   id: string;
   title: string;
   description: string;
@@ -14,24 +17,76 @@ interface PostData {
   hide: boolean;
 }
 
+interface Proposal {
+  id: string;
+  description: string;
+  price: string;
+  acepted: boolean | null;
+  status: boolean | null;
+  proposer_fk: string;
+  anounce_fk: string;
+  created_at: string;
+  quantity: string;
+}
+
+const anounceId = "373d7fda-e2a1-4248-91e3-21ae173c67b7"
 const DetailAnounce: React.FC = () => {
-  const [postData, setPostData] = useState<PostData | null>(null);
+  const [Anounce, setAnounce] = useState<Anounce | null>(null);
+  const [Proposal, setProposal] = useState<Proposal[]>([]);
 
   useEffect(() => {
-    // Faça a requisição para a URL que retorna o JSON
-    fetch('SUA_URL_AQUI')
-      .then((response) => response.json())
-      .then((data: PostData) => setPostData(data))
-      .catch((error) => console.error(error));
+    // Faça a solicitação para a URL e obtenha o JSON
+    axios
+      .get<Anounce>(`${BASE_URL}/anounce/${anounceId}`)
+      .then((response) => {
+        setAnounce(response.data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar dados:', error);
+      });
+
+    axios
+      .get<Proposal[]>(`${BASE_URL}/proposal/proposalsbyanounceid?id=${anounceId}`)
+      .then((response) => {
+        setProposal(response.data);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar dados adicionais:', error);
+      });
   }, []);
 
   return (
     <div>
-      <h1>Detalhes do Post</h1>
-      {postData ? postData.anouncer_fk : 'Carregando...'}
+      <h1>Exemplo de exibição de JSON em Next.js com TypeScript</h1>
+      {Anounce ? (
+        <div>
+          <h2>Título: {Anounce.title}</h2>
+          <p>Descrição: {Anounce.description}</p>
+          <p>Quantidade: {Anounce.quantity} {Anounce.unit}</p>
+        </div>
+      ) : (
+        <p>Carregando dados...</p>
+      )}
+<hr/>
+      <h2>Dados adicionais:</h2>
+
+      {/* Exibindo os dados adicionais da segunda URL */}
+      {Proposal.length > 0 ? (
+        <ul>
+          {Proposal.map((item) => (
+            <li key={item.id}>
+              <p>Descrição: {item.description}</p>
+              <p>Preço: {item.price}</p>
+              <p>Quantidade: {item.quantity}</p>
+              {/* Adicione mais campos conforme necessário */}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Carregando dados adicionais...</p>
+      )}
     </div>
   );
 };
 
-
-export default DetailAnounce
+export default DetailAnounce;
